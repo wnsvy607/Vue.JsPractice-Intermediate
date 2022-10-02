c82decf2809ff2a44734297762986f8d8ba906a1
wnsvy607 <wnsvy607@naver.com>
2022-10-02 16:15:48 +0900

    App.vue CSS 수정 및 TodoHeader.vue 파일에 scoped 속성으로 지역 컴포넌트 스타일 정의

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,15 +1,16 @@
 <template>
-    <div>
-        header
-    </div>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
+<!-- scoped : CSS는 Cascading Style Sheets로서 이름에 
+    붙었다시피 하위 스타일이 상위 스타일을 상속한다. 
+    하지만 scoped 속성을 통해 해당 컴포넌트만의 스타일을 정의할 수 있다.-->
+<style scoped>
+    h1 {
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
