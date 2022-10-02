eab92114df8f08101ac25f4a8ef99c10b54db092
wnsvy607 <wnsvy607@naver.com>
2022-10-02 20:20:08 +0900

    TodoList 추가 및 삭제 기능 구현 1

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,15 +1,59 @@
 <template>
-  <div>
-    input
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <span class="addContainer" v-on:click="addTodo">
+
+      <i class="fa-solid fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data: function () {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      console.log(this.newTodoItem);
+      // key - value 쌍으로 저장되는 API
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function () {
+      // input box에 입력된 값을 비우는 로직
+      this.newTodoItem = "";
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer{
+    float: right;
+    background: linear-gradient(to right, #6478FB,#8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
 </style>
