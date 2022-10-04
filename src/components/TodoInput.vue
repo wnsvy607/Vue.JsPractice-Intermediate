4ff96744d31323aef27b54777bea2cb4338aa89b
wnsvy607 <wnsvy607@naver.com>
2022-10-04 21:39:32 +0900

    트렌지션 구현

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -5,15 +5,28 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="fa-solid fa-plus addBtn"></i>
     </span>
+    <AlertModal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        경고
+        <i class="closeModal Btn fa-regular fa-circle-xmark" @click="showModal = false"></i>
+      </h3>
+      <div slot="body">
+        빈칸을 저장할 수 없습니다.
+      </div>
+      
+    </AlertModal>
   </div>
 </template>
 
 <script>
+import AlertModal from './common/AlertModal.vue';
+
 export default {
   data: function () {
     return {
       newTodoItem: "",
-    };
+      showModal: false
+    }
   },
   methods: {
     addTodo: function () {
@@ -21,13 +34,18 @@ export default {
       // this.$emit('이벤트 이름', 인자1, 인자2, ...);
       this.$emit('addTodoItem', this.newTodoItem);
       this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput: function () {
       // input box에 입력된 값을 비우는 로직
       this.newTodoItem = "";
-    },
+    }
   },
+  components: {
+    'AlertModal': AlertModal
+  }
 };
 </script>
 
@@ -55,5 +73,8 @@ export default {
   .addBtn {
     color: white;
     vertical-align: middle;
+  }
+  .closeModalBtn {
+    color: #42b983;
   }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li
         v-for="(todoItem, index) in propsdata"
         v-bind:key="todoItem.item"
@@ -18,7 +18,7 @@
           <i class="fa-solid fa-trash"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -69,5 +69,14 @@ li {
 .textCompleted {
   text-decoration: line-through;
   color: #b3adad;
+}
+
+/* 리스트 아이템 트랜지션 효과 */
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
